9ed51f3d3717df07186c9d8df6a25bee10504e18
chadwu <chad.wu014004@gmail.com>
2017-01-05 19:53:57 +0800

    Chiayi-2

--- Chiayi/travel/views.py
+++ Chiayi/travel/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from travel.models import Article, Comment
 
 
 def travel(request):
     '''
     Render the travel page
     '''
-    return render(request, 'travel/travel.html')
+    articles = Article.objects.all()
+    itemsList = []
+    for travel in articles:
+        items = [travel]
+        items.extend(list(Comment.objects.filter(article=travel)))
+        itemsList.append(items)
+    context = {'itemsList':itemsList}
+    return render(request, 'travel/travel.html', context)
